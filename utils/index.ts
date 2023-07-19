0235e61be8a277f375d61c2131e9eb6ef0b7d695
Lucas Vieira <ls6182315@gmail.com>
2023-07-19 15:24:23 -0300

    fix:dotenv var

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import 'dotenv/config'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL
+const API_URL = process.env.NEXT_PUBLIC_API_HOST
 
 export async function getBotResponse(message: string) {
   return await axios.post(`${API_URL}/predict`, {
