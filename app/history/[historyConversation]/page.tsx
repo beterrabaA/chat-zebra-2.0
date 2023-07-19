65adc96d55862ed3d190956d8cb6d706d3e3f36c
Lucas Vieira <ls6182315@gmail.com>
2023-07-19 14:45:53 -0300

    feat:add handleDownload function

--- app/history/[historyConversation]/page.tsx
+++ app/history/[historyConversation]/page.tsx
@@ -22,6 +22,22 @@ const HistoryMessage = ({ params }: { params: historyConversationProps }) => {
     setConversationList(data)
   }
 
+  const handleDowlonad = async () => {
+    setIsDownloading(true)
+    const valor = {
+      data: conversationList,
+    }
+
+    const response = await axios.post(`${API_URL}/upload`, valor)
+    const url = window.URL.createObjectURL(new Blob([response.data]))
+    const link = document.createElement('a')
+    link.href = url
+    link.setAttribute('download', `${params.historyConversation}.csv`)
+    document.body.appendChild(link)
+    link.click()
+    setIsDownloading(false)
+  }
+
   const ChatDownloadFooter = () => (
     <button
       type="button"
