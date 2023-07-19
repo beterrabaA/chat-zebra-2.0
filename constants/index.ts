cfb47bee5bbb1fe323defba0191ab6b142118f49
Lucas Vieira <ls6182315@gmail.com>
2023-07-19 14:30:16 -0300

    feat:add local reponses constants

---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+import { IMessage } from '@/types'
+
+export const messageReply = [
+  {
+    name: 'zebra',
+    message: 'To continue,you should be logged.Insert your username:',
+  },
+  {
+    name: 'zebra',
+    message: 'Insert your password:',
+  },
+  {
+    name: 'zebra',
+    message: 'Insert your first name and last name:',
+  },
+  {
+    name: 'zebra',
+    message: 'Username or password is incorrect, try again:',
+  },
+  {
+    name: 'zebra',
+    message: 'You are logged in',
+  },
+  {
+    name: 'zebra',
+    message: 'Try something like: "Hello" or "How are you"',
+  },
+  {
+    name: 'zebra',
+    message: 'Connection lost, please try again later',
+  },
+] as IMessage[]
