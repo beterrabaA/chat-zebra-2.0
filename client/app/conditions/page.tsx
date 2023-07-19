c323cc36f70f3282788754b9d0f8d6167351aea8
Lucas Vieira <ls6182315@gmail.com>
2023-07-19 20:58:16 +0000

    conditons

---
+++ client/app/conditions/page.tsx
@@ -0,0 +1,15 @@
+const page = () => {
+    return (
+      <div className="flex h-full flex-col items-center justify-center">
+        <h1 className="text-3xl font-semibold">Conditions</h1>
+        <div>
+          <p>Credits History</p>
+          <p>Account</p>
+          <p>Contact a Manager</p>
+        </div>
+      </div>
+    )
+  }
+  
+  export default page
+  
