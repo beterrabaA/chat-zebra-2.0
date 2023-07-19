5230ff76c14c905ce712f93818c9420a4a5736d1
Lucas Vieira <ls6182315@gmail.com>
2023-07-19 20:56:26 +0000

    more page

---
+++ client/app/more/page.tsx
@@ -0,0 +1,14 @@
+const page = () => {
+    return (
+      <div className="flex h-full flex-col items-center justify-center">
+        <h1 className="text-3xl font-semibold">More</h1>
+        <div>
+          <h2>More informations?</h2>
+          <p>Acess our wensite</p>
+        </div>
+      </div>
+    )
+  }
+  
+  export default page
+  
